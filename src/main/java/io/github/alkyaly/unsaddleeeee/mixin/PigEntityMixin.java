b08e8660df47a03c1a7c2605d51bb6303c784be4
adrianoy <adriano.yt624@gmail.com>
2021-06-08 11:00:32 -0300

    And I'd have gotten away with it too, if it weren't for these Meddling Kids

---
+++ src/main/java/io/github/alkyaly/unsaddleeeee/mixin/PigEntityMixin.java
@@ -0,0 +1,38 @@
+package io.github.alkyaly.unsaddleeeee.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PigEntity.class)
+public abstract class PigEntityMixin extends AnimalEntity {
+    protected PigEntityMixin(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Shadow public abstract boolean isSaddled();
+
+    @Shadow @Final private static TrackedData<Boolean> SADDLED;
+
+    @Inject(at = @At("HEAD"), method = "interactMob", cancellable = true)
+    private void removeSaddle(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        if (player.isSneaking() && isSaddled() && !hasPassengers()) {
+            dropStack(new ItemStack(Items.SADDLE));
+            dataTracker.set(SADDLED, false);
+            cir.setReturnValue(ActionResult.SUCCESS);
+        }
+    }
+}
